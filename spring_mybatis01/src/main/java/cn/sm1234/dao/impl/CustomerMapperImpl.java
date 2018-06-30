b5a198a6539a36fea5df4825aa1ed9ba9b6d60ae
ric <1162220392@qq.com>
2018-06-30 19:11:26 +0800

    first commit

---
+++ spring_mybatis01/src/main/java/cn/sm1234/dao/impl/CustomerMapperImpl.java
@@ -0,0 +1,17 @@
+package cn.sm1234.dao.impl;
+
+import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+
+import cn.sm1234.dao.CustomerMapper;
+import cn.sm1234.domain.Customer;
+
+public class CustomerMapperImpl extends SqlSessionDaoSupport implements CustomerMapper {
+
+	public void saveCustomer(Customer customer) {
+		SqlSession sqlSession = this.getSqlSession();
+		sqlSession.insert("saveCustomer",customer);
+		//这里不需要事务提交
+	}
+
+}
